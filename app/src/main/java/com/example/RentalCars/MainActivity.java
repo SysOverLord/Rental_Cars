ae145f9dbcf4e8108a2cc30913f5d08972d8b516
Batuhan <batuhanm35@gmail.com>
2021-04-16 16:39:22 +0300

    V0.2.2

--- app/src/main/java/com/example/RentalCars/MainActivity.java
+++ app/src/main/java/com/example/RentalCars/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.RentalCars;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler handler;
+    private TextView txtAppName;
+    private Animation txtViewAnimation;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_test);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
+        txtAppName = findViewById(R.id.txtAppName);
+        txtViewAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.textview_animation);
 
+        handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.postDelayed(runnable, 4500);
+
+        txtAppName.setAnimation(txtViewAnimation);
+    }
 }
 
