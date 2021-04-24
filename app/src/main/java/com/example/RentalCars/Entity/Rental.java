69289fe56ea65e2b0bb72d935f0e87585af88200
Batuhan <batuhanm35@gmail.com>
2021-04-24 16:16:09 +0300

    V0.2.6

--- app/src/main/java/com/example/RentalCars/Entity/Rental.java
+++ app/src/main/java/com/example/RentalCars/Entity/Rental.java
@@ -1,5 +1,59 @@
 package com.example.RentalCars.Entity;
 
+import java.util.Date;
+
 public class Rental {
     Car rentedCar;
+    Date startDate;
+    Date endDate;
+    float totalPrice;
+    Person renter;
+
+    public Rental(Car rentedCar, Date startDate, Date endDate, float totalPrice, Person renter) {
+        this.rentedCar = rentedCar;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.totalPrice = totalPrice;
+        this.renter = renter;
+    }
+
+    public Person getRenter() {
+        return renter;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Car getRentedCar() {
+        return rentedCar;
+    }
+
+    public void setRentedCar(Car rentedCar) {
+        this.rentedCar = rentedCar;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
